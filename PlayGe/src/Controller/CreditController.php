c2ed08941d480ea493fc13cd701b9c4bd8052aeb
Jezy123 <juanmapg321@gmail.com>
2023-02-22 10:39:06 +0100

    controlador creditos 1.01

--- PlayGe/src/Controller/CreditController.php
+++ PlayGe/src/Controller/CreditController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(path:'/game')]
 class ApiController extends AbstractController
 {
-    #[Route('/win/{id}/{ganar}', name: 'api-show',  methods: ['POST'])]
+    #[Route('/win/{id}/{ganar}', name: 'winGame',  methods: ['POST'])]
     public function win(ManagerRegistry $doctrine, $ganar ,$id): JsonResponse
     {
         $repository = $doctrine->getRepository(User::class);
@@ -22,7 +22,7 @@ class ApiController extends AbstractController
         return new JsonResponse($user -> getCreditos(), Response::HTTP_OK);
     }
 
-    #[Route('/lose/{id}/{perder}', name: 'api-show',  methods: ['POST'])]
+    #[Route('/lose/{id}/{perder}', name: 'loseGame',  methods: ['POST'])]
     public function lose(ManagerRegistry $doctrine, $perder ,$id): JsonResponse
     {
         $repository = $doctrine->getRepository(User::class);
