af247d27fc3e12ffe375c0efa23fc05717f124a9
Jezy123 <juanmapg321@gmail.com>
2023-02-22 10:36:19 +0100

    Controllador de creditos

---
+++ PlayGe/src/Controller/CreditController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(path:'/game')]
+class ApiController extends AbstractController
+{
+    #[Route('/win/{id}/{ganar}', name: 'api-show',  methods: ['POST'])]
+    public function win(ManagerRegistry $doctrine, $ganar ,$id): JsonResponse
+    {
+        $repository = $doctrine->getRepository(User::class);
+        $user = $repository->find($id);
+        $user -> setCreditos($user -> getCreditos() + $ganar);
+        
+        return new JsonResponse($user -> getCreditos(), Response::HTTP_OK);
+    }
+
+    #[Route('/lose/{id}/{perder}', name: 'api-show',  methods: ['POST'])]
+    public function lose(ManagerRegistry $doctrine, $perder ,$id): JsonResponse
+    {
+        $repository = $doctrine->getRepository(User::class);
+        $user = $repository->find($id);
+        $user -> setCreditos($user -> getCreditos() - $perder);
+        
+        return new JsonResponse($user -> getCreditos(), Response::HTTP_OK);
+    }
+}
